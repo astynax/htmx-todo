f8dd551cc45ae87254e1304df9c2b8f2bdedaece
Aleksei Pirogov <astynax@users.noreply.github.com>
2022-09-15 18:44:20 +0400

    add a minimal WebSocket example

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
+import Control.Concurrent (threadDelay)
+import Control.Monad
 import Network.Wai
 import Network.Wai.Cli (defWaiMain)
+import Network.Wai.Handler.WebSockets
+import Network.WebSockets
 import Text.Blaze.Html5 (Markup, (!))
 import Text.Blaze.Html5 qualified as H
 import Text.Blaze.Html5.Attributes qualified as A
@@ -12,7 +16,9 @@ import XStatic
 import XStatic.TH
 
 main :: IO ()
-main = defWaiMain =<< scottyApp app
+main = defWaiMain . withWS =<< scottyApp app
+  where
+    withWS = websocketsOr defaultConnectionOptions wsApp
 
 app :: ScottyM ()
 app = do
@@ -32,18 +38,34 @@ index = H.docTypeHtml do
       ! A.src "/xstatic/htmx.min.js"
       $ pure ()
   H.body do
+    ticker
     here
 
 here :: Markup
 here =
-  H.div
+  H.span
   ! H.customAttribute "hx-get" "/enter"
   ! H.customAttribute "hx-trigger" "mouseenter once"
   $ "[ Here Mouse, Mouse! ]"
 
 gotYa :: Markup
 gotYa =
-  H.div
+  H.span
   ! H.customAttribute "hx-get" "/leave"
   ! H.customAttribute "hx-trigger" "mouseleave once"
   $ "[ Got ya! ]"
+
+ticker :: Markup
+ticker =
+  H.span
+  ! H.customAttribute "hx-ws" "connect:/"
+  $ H.span ! A.id "ticker" $ "..."
+
+wsApp :: ServerApp
+wsApp pendingConn = do
+  conn <- acceptRequest pendingConn
+  forever do
+    sendTextData conn . renderHtml $ H.span ! A.id "ticker" $ "> tick <"
+    threadDelay 1000000
+    sendTextData conn . renderHtml $ H.span ! A.id "ticker" $ "< tock >"
+    threadDelay 1000000
